cb3f5185cfbe66496ebbfd9421d64da009d98960
stanleyxu7 <90428695+stanleyxu7@users.noreply.github.com>
2021-09-09 17:47:15 -0700

    Recitation 1
    Recitation 1 Thonny Practice Assignment

---
+++ demo.py
@@ -0,0 +1,24 @@
+print(2+6)
+print(6-2)
+print(6*2)
+print(6/2)
+print(6%2)
+print(6**2)
+print(6+10*10+6)
+print((6+10)*(10+6))
+print(7+10-(2/2))
+print((100%2)+1)
+print((2**2)+(9/3))
+print("square of 7 is ", 7**2)
+a=5
+b=3.2
+c="Hello"
+print("type of variable a is", type(a))
+print("type of variable b is", type(b))
+print("type of variable c is", type(c))
+points_obtained=10
+total_points=100
+percentage=(points_obtained/total_points)*100
+print(points_obtained)
+print(total_points)
+print("percentage are", percentage, "%")
